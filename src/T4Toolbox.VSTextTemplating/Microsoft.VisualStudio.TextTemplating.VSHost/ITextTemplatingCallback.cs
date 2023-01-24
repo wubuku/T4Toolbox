adc518e76fe5dec1be9e506070bd1a5fbdfcc29d
Yang Jiefeng <>
2023-01-24 21:22:34 +0800

    refactor solution

--- src/T4Toolbox.VSTextTemplating/Microsoft.VisualStudio.TextTemplating.VSHost/ITextTemplatingCallback.cs
+++ src/T4Toolbox.VSTextTemplating/Microsoft.VisualStudio.TextTemplating.VSHost/ITextTemplatingCallback.cs
@@ -2,22 +2,6 @@
 
 namespace Microsoft.VisualStudio.TextTemplating.VSHost
 {
-    using Microsoft.VisualStudio.TextTemplating;
-
-    public interface ITextTemplatingComponents
-	{
-        ITextTemplatingEngineHost Host { get; }
-
-        object Hierarchy { get; set; }
-
-        ITextTemplatingCallback Callback { get; set; }
-
-        ITextTemplatingEngine Engine { get; }
-
-        string InputFile { get; set; }
-    }
-
-    
     public interface ITextTemplatingCallback
     {
         void ErrorCallback(bool warning, string message, int line, int column);
---
+++ src/T4Toolbox.VSTextTemplating/Microsoft.VisualStudio.TextTemplating.VSHost/ITextTemplatingComponents.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Microsoft.VisualStudio.TextTemplating.VSHost
+{
+    using Microsoft.VisualStudio.TextTemplating;
+
+    public interface ITextTemplatingComponents
+	{
+        ITextTemplatingEngineHost Host { get; }
+
+        object Hierarchy { get; set; }
+
+        ITextTemplatingCallback Callback { get; set; }
+
+        ITextTemplatingEngine Engine { get; }
+
+        string InputFile { get; set; }
+    }
+
+}
+    
